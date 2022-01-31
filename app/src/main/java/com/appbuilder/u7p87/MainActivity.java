8c532d4554014b12d13bed106e141323268b1cd8
rbobroff <r.bobroff@gmail.com>
2022-01-31 20:15:10 +0300

    Добавлен комментарий в MainActivity.java - ссылка на урок по созданию иконки

--- app/src/main/java/com/appbuilder/u7p87/MainActivity.java
+++ app/src/main/java/com/appbuilder/u7p87/MainActivity.java
@@ -1,5 +1,7 @@
 package com.appbuilder.u7p87;
 
+//Инструкция по иконке приложения: https://www.youtube.com/watch?v=96TAF20qPH0
+//Экран загрузки приложения: 
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
