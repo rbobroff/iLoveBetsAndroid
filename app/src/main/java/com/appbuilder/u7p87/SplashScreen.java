5973623ade5a4b9f7413d7c9b3136af3967ee6f3
rbobroff <r.bobroff@gmail.com>
2022-02-01 22:44:44 +0300

    SplashScreen. Добавлены Activity (xml) и класс для этой активити (java).

---
+++ app/src/main/java/com/appbuilder/u7p87/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.appbuilder.u7p87;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
