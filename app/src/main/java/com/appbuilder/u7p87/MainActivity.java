01820b062d2949f87ac65d5f65b7151e5bdef3a2
rbobroff <r.bobroff@gmail.com>
2022-02-01 22:45:08 +0300

    Добавлен код для работы SplashScreen

--- app/src/main/java/com/appbuilder/u7p87/MainActivity.java
+++ app/src/main/java/com/appbuilder/u7p87/MainActivity.java
@@ -1,7 +1,8 @@
 package com.appbuilder.u7p87;
 
 //Инструкция по иконке приложения: https://www.youtube.com/watch?v=96TAF20qPH0
-//Экран загрузки приложения: 
+//Экран загрузки приложения: 1) https://www.youtube.com/watch?v=Zlo3qpkZ1aI&t=257s 2) Или Урок 5 - geekbrains. Android 1. 1ч 29 мин 13 с
+
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
