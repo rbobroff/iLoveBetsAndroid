47e3130bf0b9d49d82a1a909eb4e462644c634b7
rbobroff <r.bobroff@gmail.com>
2022-02-12 19:32:00 +0300

    Добавлены переходы на bettingTips и help. Файлы

---
+++ app/src/main/java/com/appbuilder/u7p87/BettingTipsActivity.java
@@ -0,0 +1,14 @@
+package com.appbuilder.u7p87;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BettingTipsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_betting_tips);
+    }
+}
---
+++ app/src/main/java/com/appbuilder/u7p87/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.appbuilder.u7p87;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
